9036e66b8da8d2e432c11cfb6090302828bafdc7
Techh Jork <62848979+techhjork@users.noreply.github.com>
2020-05-28 11:59:49 +0530

    Update user.php

--- user.php
+++ user.php
@@ -58,27 +58,24 @@ header("Location: user.php");
 </table>
 </div>
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal">
+
+<div class="modal fade" id="myModal">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <button type="button" class="close" data-dismiss="modal">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-       <?php 
-       if($_GET['id']){
-        $id = $_GET['id'];
-        echo $id;
-        }else{
-        	$id ="1";
-        }
-       $update = new Crud;
-       $update->fetch_update($id);
-       ?>
+			<?php 
+			$id = 1;
+			if(isset($_GET) && $_GET){
+			$update = new Crud;
+			$update->fetch_update($id);
+			 }
+			?>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
